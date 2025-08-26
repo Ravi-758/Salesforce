32e97de898b0e5ef2488bd3cab6d4c3a2d93e757
ravi-756 <ravidhundhara2000.gmail>
2025-08-26 17:52:03 +0530

    final

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -3,7 +3,8 @@ import { LightningElement } from 'lwc';
 export default class BmiCalculator extends LightningElement {
     height = '';
     weight = '';
-
+    bmiValue ='';
+    result = '';
     inputHandler(event){
         const{name,value} = event.target;
         if(name === "height"){
@@ -14,8 +15,33 @@ export default class BmiCalculator extends LightningElement {
         }
     }
 
-    submitHander(event){
+    submitHandler(event){
         event.preventDefault();
         console.log(this.height,this.weight);
+        this.calculate();
+
     }
+    calculate(){
+        let height = Number(this.height);
+        let bmi = Number(this.weight)/(height*height);
+        console.log(bmi);
+        this.bmiValue = Number(bmi.toFixed(2));
+
+        if(this.bmiValue<18.5){
+            this.bmiValue = 'Underweight';
+        }else if(this.bmiValue>=18.5 && this.bmiValue<25){
+            this.result = 'Normal';
+        }else if(this.bmiValue>=25 && this.bmiValue<30){
+            this.result = 'Overweight';
+        }else {
+            this.result = 'Obese';
+        }
+    }
+    recalculate(){
+        this.result = '';
+        this.bmiValue = '';
+        this.weight= '';
+        this.height ='';
+    }
+
 }
