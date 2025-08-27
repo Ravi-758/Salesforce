52a1c701e150a7111ca88dc2f61dca5d4d1ea508
ravi-756 <ravidhundhara2000.gmail>
2025-08-27 23:55:56 +0530

    alarmclock logic

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
+import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets';
+export default class AlarmClockApp extends LightningElement {
+    clockImage = AlarmClockAssests+'/AlarmClockAssets/clock.png';
+    currentTime = `${hour}:${minutes}:${seconds} ${ampm}`
+    connectedCallback(){
+        this.currectTimeHandler();
+    }
+    currectTimeHandler(){
+        setInterval(()=>{
+             let date = new Date();
+        let hours = dateTime.getHours();
+        let minutes = dateTime.getMinutes();
+        let seconds = dateTime.getSeconds();
+        let ampm = "AM"
 
-export default class AlarmClockApp extends LightningElement {}
+        if(hour == 0){
+            hour = 12;
+        }else if(hour > 12){
+            hour = hour - 12;
+            ampm = "PM";
+        }
+        hour = hour<10 ? "0"+hour : hour;
+        minutes = minutes<10 ? "0"+minutes : minutes;
+        seconds = seconds<10 ? "0"+seconds : seconds;
+        },1000)
+       
+
+        }
+}
